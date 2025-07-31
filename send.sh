998416319b7183d664e38ea7ebddc831cf9f285c
seo <seo@MacBook-Pro.local>
2025-07-31 22:09:59 +0900

    Remove send.sh

--- send.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-scp -P 2222 CVE-2019-8605/exp seo@192.168.22.130:~/vulntest/CVE-2019-8605 && \
-scp -P 2222 CVE-2019-6225/poc seo@192.168.22.130:~/vulntest/CVE-2019-6225
-
-
-ssh -p 2222 seo@192.168.22.130 'sync; sync; sync; sync; sync; sync; sync; sync; sync;'
